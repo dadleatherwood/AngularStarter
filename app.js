809c87be17c3c9c01eaddb6c7a0213af625a60b4
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-05-30 11:56:03 -0500

    added fiter to determine if food was spicy or not

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 angular.module('angularTest', [])
 
-.controller('TestController', function($scope) {
-  $scope.name = 'Jacob'
-});
+  .controller('TestController', function($scope, foodService) {
+    $scope.name = 'Jacob';
+    $scope.foods = foodService.getList();
+
+    $scope.spicyFoods = $scope.foods.filter(function(val){
+      return val.spicy;
+    })
+  })
+
+  .service('foodService', function() {
+    var privateVar = 'this is private' //private
+
+    //makes public or viewable
+    this.list = [
+      {
+        name: 'Burger',
+        origin: 'America',
+        calories: 480,
+        spicy: true
+      },
+      {
+        name: 'Pizza',
+        origin: 'Chicago',
+        calories: 10000000,
+        spicy: false
+      }
+    ];
+
+    this.getList = function () {
+      return this.list;
+    }
+
+  })
